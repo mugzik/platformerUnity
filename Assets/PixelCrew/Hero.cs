2506aa6dbef7a3d6c908e7ce003f6ca31c81769d
Maksim Rafalchuk <qlolwtf@gmail.com>
2022-11-19 02:36:22 +0300

    Created scripts which realise hero's movement behavior

---
+++ Assets/PixelCrew/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private Vector2 _direction;
+    [SerializeField] private float _speed;
+
+    public void SetDirection(Vector2 direction)
+    {
+        _direction = direction;
+    }
+
+    public void BattleRoar()
+    {
+        Debug.Log("ROOOOARRR!!!");
+    }
+
+    private void Update()
+    {
+        if( _direction != new Vector2(0,0) )
+        {
+            var d_x = _direction.x * _speed * Time.deltaTime;
+            var d_y = _direction.y * _speed * Time.deltaTime;
+            var newXPos = transform.position.x + d_x;
+            var newYPos = transform.position.y + d_y;
+
+            transform.position = new Vector3( newXPos, newYPos, transform.position.z );
+        }
+    }
+}
