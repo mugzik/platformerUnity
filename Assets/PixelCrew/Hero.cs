63a3a797e7e8303dbf126f4cfe13286b668cc5de
Maksim Rafalchuk <qlolwtf@gmail.com>
2022-11-21 04:41:28 +0300

    Added physics, collisions and jumping

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -4,9 +4,48 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
     private Vector2 _direction;
-    [SerializeField] private float _speed;
 
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerCheck _groundCheck;
+
+    // Private methods
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var isJumping = _direction.y > 0;
+
+        _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+        if (isJumping)
+        {
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            }
+        }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        // GroundCheck method
+        // Return true if object 'hero' have 'bottom' collision with layers from _groundCheck._groundLayer
+        // overwise return false. See LayerCheck class
+
+        return _groundCheck.IsTouchingLayer();
+    }
+
+    // Public methods
     public void SetDirection(Vector2 direction)
     {
         _direction = direction;
@@ -17,16 +56,4 @@ public class Hero : MonoBehaviour
         Debug.Log("ROOOOARRR!!!");
     }
 
-    private void Update()
-    {
-        if( _direction != new Vector2(0,0) )
-        {
-            var d_x = _direction.x * _speed * Time.deltaTime;
-            var d_y = _direction.y * _speed * Time.deltaTime;
-            var newXPos = transform.position.x + d_x;
-            var newYPos = transform.position.y + d_y;
-
-            transform.position = new Vector3( newXPos, newYPos, transform.position.z );
-        }
-    }
 }
